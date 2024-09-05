354778e8d10cbd993c9994362c06659a5dbc8447
PandaTree <112433106+rkardoes@users.noreply.github.com>
2024-09-04 19:26:33 -0600

    Add files via upload

---
+++ nSquaredPolyMult.py
@@ -0,0 +1,11 @@
+def poly_mult(coA: list, coB: list):
+    ans = [0] * (len(coA) + len(coB) - 1)
+    for x, a in enumerate(coA):
+        for y, b in enumerate(coB):
+            ans[x+y] += (a*b)
+    return ans
+
+A = [7,2,3]
+B = [4,-1,2]
+
+print(poly_mult(A, B))
